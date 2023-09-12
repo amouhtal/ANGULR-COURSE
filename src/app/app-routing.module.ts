e4b58ecd78ff7309b6693cf75459347fe24bbef1
amouhtal <61628581+amouhtal@users.noreply.github.com>
2023-09-12 12:14:12 +0100

    Setup router, Router configuration, router-outlet, routerLink property

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeComponent } from './employee/employee.component';
+import { RoomsComponent } from './rooms/rooms.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'employee', component: EmployeeComponent },
+  { path: 'rooms', component: RoomsComponent },
+  // { path: '', redirectTo: '/rooms', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { InitService } from './init.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'angular-course';
