0932a738b526eefde08207c9a75baa2489c01825
amouhtal <61628581+amouhtal@users.noreply.github.com>
2023-10-03 18:30:35 +0100

    Introduction,  Using Material Controls, Nested Form Controls, Dynamic Forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,16 +7,22 @@ import { LoginGuard } from './guards/login.guard';
 
 const routes: Routes = [
   { path: 'employee', component: EmployeeComponent, canActivate: [LoginGuard] },
-  { path: 'booking', loadChildren: () => import('./booking/booking.module').then(m => m.BookingModule)
-  , canActivate: [LoginGuard] },
+  { path: 'login', component: LoginComponent },
+
   {
     path: 'rooms',
-    loadChildren: () => import('./rooms/rooms.module').then(m => m.RoomsModule),
+    loadChildren: () =>
+      import('./rooms/rooms.module').then((m) => m.RoomsModule),
     canActivate: [LoginGuard],
     canLoad: [LoginGuard]
-  },
-  { path: 'login', component: LoginComponent },
+  }, 
   { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {
+    path: 'booking',
+    loadChildren: () =>
+      import('./booking/booking.module').then((m) => m.BookingModule),
+    // , canActivate: [LoginGuard]
+  },
   { path: '**', component: NotfoundComponent },
 ];
 
--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,16 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { ConfigService } from '../services/config.service';
+import { FormGroup, FormBuilder, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
-  styleUrls: ['./booking.component.scss']
+  styleUrls: ['./booking.component.scss'],
 })
 export class BookingComponent implements OnInit {
-
-  constructor(private configService: ConfigService) { }
+  bookingForm!: FormGroup;
+  guests = this.bookingForm.get('guests') as FormArray;
+  constructor(private configService: ConfigService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.bookingForm = this.fb.group({
+      roomId: new FormControl({ value: '2', disabled: true }),
+      checkInDate: new FormControl(''),
+      checkOutDate: new FormControl(''),
+      bookingStatus: new FormControl(''),
+      bookingAmount: new FormControl(''),
+      bookingDate: new FormControl(''),
+      mobileNumber: new FormControl(''),
+      guestEmail: new FormControl(''),
+      guestName: new FormControl(''),
+      guestCount: new FormControl(''),
+      address: this.fb.group({
+        AdressLine1: new FormControl(''),
+        AdressLine2: new FormControl(''),
+        city: new FormControl(''),
+        state: new FormControl(''),
+        country: new FormControl(''),
+        zipCode: new FormControl(''),
+      }),
+      guests: this.fb.array([
+        this.fb.group({
+          guestName: new FormControl(''),
+          age: new FormControl(''),
+        }),
+      ]),
+    });
   }
 
+  addBooking() { 
+    console.log('values', this.bookingForm.getRawValue());
+
+    this.bookingForm.reset();
+  }
+  addGuest() {
+    this.guests.controls.push();
+  }
 }
